38e25ab1f0cb0e7b468b7e4a41cb8c6df45d6040
Ryanu01 <ryangomes08090109@gmail.com>
2026-01-19 11:39:08 +0530

    post dsa question endpoint done!

--- types.ts
+++ types.ts
@@ -1,22 +1,22 @@
-import { email, z } from "zod"
+import { z } from "zod"
 
 export const SignInSchema = z.object({
-    name: z.string(),
-    email: z.email(),
-    password: z.string().min(6),
-    role: z.enum(["creator", "contestee"])
+  name: z.string(),
+  email: z.email(),
+  password: z.string().min(6),
+  role: z.enum(["creator", "contestee"])
 })
 
 export const LoginSchema = z.object({
-    email: z.email(),
-    password: z.string()
+  email: z.email(),
+  password: z.string()
 })
 
 export const ContestSchema = z.object({
-    title: z.string(),
-    description: z.string(),
-    startTime: z.coerce.date(),
-    endTime: z.coerce.date()
+  title: z.string(),
+  description: z.string(),
+  startTime: z.coerce.date(),
+  endTime: z.coerce.date()
 })
 
 export const McqSchema = z.object({
@@ -27,5 +27,23 @@ export const McqSchema = z.object({
 })
 
 export const McqSubmissionSchema = z.object({
-    selectedOptionIndex: z.number()
+  selectedOptionIndex: z.number()
 })
+
+const testCasesSchema = z.object({
+  input: z.string(),
+  expectedOutPut: z.string(),
+  isHeaden: z.boolean()
+})
+
+export const DsaSchma = z.object({
+  title: z.string(),
+  description: z.string(),
+  tags: z.string().array(),
+  points: z.number(),
+  timeLimit: z.number(),
+  memoryLimit: z.number(),
+  testCases: testCasesSchema.array()
+})
+
+
